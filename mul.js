4b9fb3311c01fe0fc754391355eb4317d097d5d8
backEndFriend51 <59559655+backEndFriend51@users.noreply.github.com>
2020-03-18 11:49:34 +0100

    Add .js- file via uplaod
    function mul(a, b) - Schleifen und Funktionen

---
+++ mul.js
@@ -0,0 +1,9 @@
+function mul(a, b) {
+			if(a != undefined && b != undefined) {
+				return a * b;
+			} else {
+				alert("Die Anzahl der Parameter stimmt nicht !");	
+			}
+		} 
+		
+	alert(mul(3));
